342aa3ef5efc17bbf7df222abe30d433cfe48e9e
Maria Eduarda Aires <104572741+marizzxxxx@users.noreply.github.com>
2022-12-15 15:49:19 -0300

    Add files via upload

---
+++ raio.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+float raio(float r, float a){
+
+return a = 3.1415926535898 * r * 2;
+}
+
+//marizzxxx
+
+int main(){
+	float r, a;
+	
+	printf("Programa que faz uma função que receba o raio de um círculo e retorne a sua área.\n\n");
+	
+	printf("Informe o raio do círculo: ");
+	scanf("%f", &r);
+	
+	a = raio(r, a);
+	printf("A área do círculo é: %f\n", a);
+	
+	return 0;
+}
